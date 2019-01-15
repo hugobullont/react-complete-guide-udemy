10ef3e846c744834ee05ffab96f57fe4297b9a7b
hugobullont <hugobullont@gmail.com>
2019-01-14 23:35:59 -0500

    Lesson added

--- src/App.js
+++ src/App.js
@@ -12,25 +12,32 @@ class App extends Component {
     otherState: 'some other value'
   }  
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log('Was clicked!');
     //DON'T DO THIS: this.state.persons[0].name = 'Hugo Alejandro';
     this.setState({persons: [
-      { name: 'Hugo Alejandro', age: 21 },
+      { name: newName, age: 21 },
       { name: 'Victor', age: 19 },
       { name: 'Rebeca', age:20 }
     ] });
   }
-
+  //can use arrow funtions inside onClick but is better use .bind
   render() {
      return (
        <div className="App">
          <h1>Hi, I'm a React App</h1>
          <p>This is really working!</p>
-         <button onClick={this.switchNameHandler}>Switch Name</button>
-         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+         <button onClick={() => this.switchNameHandler('Hugh Alexander!!')}>Switch Name</button>
+         <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age}/>
+         <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this,'Hugo!')}>My Hobbies: Racing</Person>
+         <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age}/>
        </div>       
      );
     //return React.createElement('div',{className: 'App'},React.createElement('h1',null, 'Does this work now?'));
